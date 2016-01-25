d4b26bb2078d20739f0ee2ac72368026ba435726
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-01-25 14:50:24 +0000

    more work on add workout view and implemented some shaky calendar logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -89,45 +89,117 @@ angular.module('starter.controllers', ['starter.services'])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
+// .controller('CalendarCtrl', function($scope, $ionicSideMenuDelegate, $stateParams) {
+//   //by default start with the current month of the year
+//   $scope.$on('$ionicView.enter', function() {
+//        // Code you want executed every time view is opened
+//        $ionicSideMenuDelegate.canDragContent(false);
+//        console.log('moment', moment());
+//   });
+//   var cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+//   var month_labels = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//   var today = new Date();
+//   $scope.calendar = Calendar(today.getMonth(), today.getFullYear());
+
+//   //this should return the details for a particular month in a year
+//   function Calendar (month, year) {
+//     var firstDay = new Date(year, month, 1);
+//     var startingDay = firstDay.getDay();
+//     return {
+//       month: month,
+//       month_label: month_labels[month],
+//       year, year
+//     }
+//   }
+
+//   $scope.nextMonth = function() {
+//     console.log("next month called")
+//     var calendar = $scope.calendar
+//     var year = (calendar.month <= 10) ? calendar.year : calendar.year + 1
+//     var month = (calendar.year === year) ? calendar.month + 1 : 0
+//     $scope.calendar = Calendar(month, year);
+//   }
+
+//   $scope.lastMonth = function() {
+//     console.log("last month called")
+//     var calendar = $scope.calendar
+//     var year = (calendar.month >= 1) ? calendar.year : calendar.year - 1
+//     var month = (calendar.year === year) ? calendar.month - 1 : 11
+//     $scope.calendar = Calendar(month, year);
+//   }
+
+// })
+
 .controller('CalendarCtrl', function($scope, $ionicSideMenuDelegate, $stateParams) {
-  //by default start with the current month of the year
   $scope.$on('$ionicView.enter', function() {
        // Code you want executed every time view is opened
        $ionicSideMenuDelegate.canDragContent(false);
   });
-  var cal_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  var month_labels = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var today = new Date();
-  $scope.calendar = Calendar(today.getMonth(), today.getFullYear());
+  // $scope.day = moment();
+  $scope.selected = _removeTime($scope.selected || moment());
+  $scope.month = $scope.selected.clone();
 
-  //this should return the details for a particular month in a year
-  function Calendar (month, year) {
-    var firstDay = new Date(year, month, 1);
-    var startingDay = firstDay.getDay();
-    return {
-      month: month,
-      month_label: month_labels[month],
-      year, year
+  var start = $scope.selected.clone();
+  start.date(1);
+  _removeTime(start.day(0));
+
+  _buildMonth($scope, start, $scope.month);
+
+  $scope.select = function(day) {
+      $scope.selected = day.date;  
+  };
+
+  $scope.next = function() {
+    console.log('moment', moment());
+      var next = $scope.month.clone();
+
+      _removeTime(next.month(next.month()+1)).date(1)
+
+      $scope.month.month($scope.month.month()+1);
+      _buildMonth($scope, next, $scope.month);
+  };
+
+  $scope.previous = function() {
+      var previous = $scope.month.clone();
+      _removeTime(previous.month(previous.month()-1).date(1));
+      $scope.month.month($scope.month.month()-1);
+      _buildMonth($scope, previous, $scope.month);
+  };
+
+  function _removeTime(date) {
+        return date.day(0).hour(0).minute(0).second(0).millisecond(0);
     }
-  }
 
-  $scope.nextMonth = function() {
-    console.log("next month called")
-    var calendar = $scope.calendar
-    var year = (calendar.month <= 10) ? calendar.year : calendar.year + 1
-    var month = (calendar.year === year) ? calendar.month + 1 : 0
-    $scope.calendar = Calendar(month, year);
-  }
+    function _buildMonth($scope, start, month) {
+        $scope.weeks = [];
+        var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
+        while (!done) {
+            $scope.weeks.push({ days: _buildWeek(date.clone(), month) });
+            date.add(1, "w");
+            done = count++ > 2 && monthIndex !== date.month();
+            monthIndex = date.month();
+        }
+    }
 
-  $scope.lastMonth = function() {
-    console.log("last month called")
-    var calendar = $scope.calendar
-    var year = (calendar.month >= 1) ? calendar.year : calendar.year - 1
-    var month = (calendar.year === year) ? calendar.month - 1 : 11
-    $scope.calendar = Calendar(month, year);
-  }
+    function _buildWeek(date, month) {
+        var days = [];
+        for (var i = 0; i < 7; i++) {
+            days.push({
+                name: date.format("dd").substring(0, 1),
+                number: date.date(),
+                isCurrentMonth: date.month() === month.month(),
+                isToday: date.isSame(new Date(), "day"),
+                date: date
+            });
+            date = date.clone();
+            date.add(1, "d");
+        }
+        return days;
+    }
 
 })
+
+
 
 .controller('WorkoutCtrl', ['$scope', '$ionicSideMenuDelegate', '$ionicModal', 'exercisesService',
   function($scope, $ionicSideMenuDelegate, $ionicModal, exercisesService) {
@@ -161,6 +233,7 @@ angular.module('starter.controllers', ['starter.services'])
       if (!alreadyAdded(exercise)) {
         console.log("Checking to see if in workout")
         console.log(JSON.stringify(exercise))
+        exercise.sets = [];
         $scope.workout.push(exercise);
       }
       $scope.exercises_modal.hide();
@@ -174,5 +247,23 @@ angular.module('starter.controllers', ['starter.services'])
     $scope.closeModal = function() {
       $scope.exercises_modal.hide();
     };
+
+    $scope.addSet = function(exercise) {
+      var reps
+      if (exercise.sets.length > 0) {
+        reps = exercise.sets[exercise.sets.length-1].reps;
+      }
+      exercise.sets.push({reps: reps});
+    }
+
+    $scope.increment = function(set) {
+      if (!set.reps) set.reps = 0;
+      console.log('increment called')
+      set.reps++;
+    }
+
+    $scope.decrement = function(set) {
+      if (set.reps > 0) set.reps--;
+    }
 
 }])
